6f01fb5558297688741f1a79b299ba8afc32712d
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-12-10 17:55:33 +0330

    Deploy a mock Chainlink VRF

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,8 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {Script} from 'forge-std/Script.sol';
+import {VRFCoordinatorV2_5Mock} from "@chainlink/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstances {
+    /** VRG Mock Values */
+    uint96 public constant MOCK_BASE_FEE = 0.25 ether;
+    uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
+    // LINK / ETH price
+    int256 public constant MOCK_WEI_PER_UNIT_LINK = 4e15;
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -28,11 +35,11 @@ contract HelperConfig is Script, CodeConstances {
     }
 
     function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
-        if(networkConfigs[chainId].vrfCoordinator != address(0)) {
+        if(networkConfigs[chainId].vrfCoordinator != address(0))
             return networkConfigs[chainId];
-        } else if (chainId == LOCAL_CHAIN_ID) {
-            //getOrCreateAnvilEthConfig
-        } else revert HelperConfig__InvalidChainId()
+        else if (chainId == LOCAL_CHAIN_ID)
+            return getOrCreateAnvilEthConfig();
+        else revert HelperConfig__InvalidChainId();
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
@@ -42,13 +49,33 @@ contract HelperConfig is Script, CodeConstances {
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B, // https://docs.chain.link/vrf/v2-5/supported-networks#sepolia-testnet
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
-            subscriptionId: 0,
-        })
+            subscriptionId: 0
+        });
     }
     
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         // check to see if we set an active network config
         if(localNetworkConfig.vrfCoordinator != address(0))
             return localNetworkConfig;
+
+        // Deploy mocks and such
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UNIT_LINK,
+        );
+        vm.stopBropadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinatorMock), // https://docs.chain.link/vrf/v2-5/supported-networks#sepolia-testnet
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000,
+            subscriptionId: 0
+        });
+
+        return localNetworkConfig;
     }
 }
